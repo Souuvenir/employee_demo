67cc2034225d751f1b30e17ff89d805477a21489
Souuvenir <nathabusnego@gmail.com>
2023-08-19 23:59:13 -0400

    feat: change in type of response to Emloyee

--- src/main/java/com/example/employee_demo/controller/EmployeeController.java
+++ src/main/java/com/example/employee_demo/controller/EmployeeController.java
@@ -1,23 +1,25 @@
 package com.example.employee_demo.controller;
 
-import com.example.employee_demo.models.EmployeeModel;
-import com.example.employee_demo.service.EmployeeService;
+import com.example.employee_demo.models.Employee;
+import com.example.employee_demo.controller.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class EmployeeController {
+    @Autowired
     EmployeeService employeeService;
 
     @PostMapping(path="/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestBody EmployeeModel employee) {
+    public @ResponseBody Employee addNewUser (@RequestBody Employee employee) {
 
       return employeeService.save(employee);
     }
 
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<EmployeeModel> getAllUsers() {
+    public @ResponseBody Iterable<Employee> getAllUsers() {
         // This returns a JSON or XML with the users
         return employeeService.findAll();
     }
