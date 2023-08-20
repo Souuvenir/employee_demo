3cb5480155ce424aa05fc21b46ec5f2c89925922
Souuvenir <nathabusnego@gmail.com>
2023-08-20 00:01:10 -0400

    refactor: change in method type

---
+++ src/main/java/com/example/employee_demo/controller/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.employee_demo.controller.service;
+
+import com.example.employee_demo.models.Employee;
+import com.example.employee_demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee save(Employee employee){
+        employeeRepository.save(employee);
+        return employee;
+    }
+
+    public Iterable<Employee> findAll(){
+        Iterable<Employee> all;
+        all = employeeRepository.findAll();
+        return all;
+    }
+}
