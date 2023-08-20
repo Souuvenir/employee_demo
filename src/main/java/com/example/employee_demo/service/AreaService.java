7e13c40b20ec1ae2ca4dd7a78fa09890ddba706b
Souuvenir <nathabusnego@gmail.com>
2023-08-20 16:45:13 -0400

    feat: area added to project

---
+++ src/main/java/com/example/employee_demo/service/AreaService.java
@@ -0,0 +1,23 @@
+package com.example.employee_demo.service;
+
+import com.example.employee_demo.models.Area;
+import com.example.employee_demo.repository.AreaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AreaService {
+    @Autowired
+    private AreaRepository areaRepository;
+
+    public Area save(Area area){
+        areaRepository.save(area);
+        return area;
+    }
+
+    public Iterable<Area> findAll(){
+        Iterable<Area> all;
+        all = areaRepository.findAll();
+        return all;
+    }
+}
